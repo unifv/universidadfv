02e503eb582f7c09a23a145c8010c1840f8c27d8
Universidad Francisco de Vitoria <43338809+unifv@users.noreply.github.com>
2022-08-22 13:54:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Universidad Francisco de Vitoria</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Next.js Toolbox" />
-        <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
-        <FeedbackForm />
-        <JokeBlock />
+        <p>Universidad Privada en Madrid, Pozuelo. Más del 30% alumnos becados. Líderes en trato personal.</p>
+    <p>La formación de la UFV dota al alumno de los conocimientos teóricos necesarios para el desempeño de su profesión, le forma en las habilidades, competencias y talentos imprescindibles para su ejercicio y le transmite los valores y actitudes necesarios para ser un gran profesional el día de mañana.</p>
+    <p>Abierta a las personas -sin limitación por razones culturales, sociales o económicas-, y al conocimiento, a las nuevas tecnologías, la ciencia, la investigación, la cultura y el cambio.</p>
+    <a href="https://www.ufv.es">https://www.ufv.es</a><br />
       </main>
       <Footer />
     </div>
